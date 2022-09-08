0141e94638c0e1ad3793d74804a64bc4bd83f131
OPApple <80098382+OPApple@users.noreply.github.com>
2022-09-08 19:43:57 +0300

    Added functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const Parser = require('expr-eval').Parser;
 const parser = new Parser()
 
 var x;
+var iterations;
 
 const toMath = (text) =>{
   return(parser.parse(text))
@@ -18,9 +19,9 @@ const findPos = (func) =>{
     }
   }
 }
-
 const findNeg = (func) =>{
   for(let i = -100; i <= 100; i++){
+    
     if(func.evaluate({x: i}) < 0){
       return(i)
     }
@@ -29,13 +30,33 @@ const findNeg = (func) =>{
 
 //Bisection interval is [a, b]
 const bisect = (func, a, b) =>{
+  var c;
+
+  for(var i = 0; i <= 100; i++){
+
+    c = (a + b)/2
+    console.log(c)  
   
+  
+    if (func.evaluate({x: a}) * func.evaluate({x: c}) < 0){
+      b = c
+    }else{
+      a = c
+    }
+
+
+
+  }
+  return(c.toFixed(3))
+
 }
 
 const clicked = (event) =>{
   x = document.getElementById('input').value
   x = toMath(x)
-  console.log(x.evaluate({x: 5}))
+  console.log(bisect(x, findNeg(x), findPos(x)))
+  var result = "x=" + bisect(x, findNeg(x), findPos(x))
+  document.getElementById('result').innerHTML = result
 }
 
 
@@ -45,6 +66,7 @@ function App() {
     <div className="App">
       <Input name="moi" inputName="input" btnText="moi" onClick={ (event) => {clicked(event)} }/>
 
+      <sapn id='result'></sapn>
       
       
     </div>
